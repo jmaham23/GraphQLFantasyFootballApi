c99e74d771e2510f45856fe8efa85a0d5d57df57
jmaham23 <48227683+jmaham23@users.noreply.github.com>
2022-06-18 21:17:08 -0500

    Gitlab is way better than github

--- src/main/java/com/jmaham/fantasy/repositories/WRRepository.java
+++ src/main/java/com/jmaham/fantasy/repositories/WRRepository.java
@@ -2,6 +2,5 @@ package com.jmaham.fantasy.repositories;
 
 import com.jmaham.fantasy.types.WR;
 import org.springframework.data.jpa.repository.JpaRepository;
-//test
 public interface WRRepository extends JpaRepository<WR, String> {
 }
